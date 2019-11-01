0b063e03066db1ff0778047f807fab8ddd187a5e
negibouze <itathirdchildnegi@gmail.com>
2019-11-01 23:49:57 +0900

    second commit
    Hello World.

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+export default {
+  mode: 'universal',
+  srcDir: 'src',
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
+  }
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <hello name="World" />
+</template>
+
+<script>
+import Hello from '~/components/atoms/Hello/Hello.vue'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
